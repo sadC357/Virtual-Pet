9300642b45059ef5933a224b736fbc0d478f6682
sadC357 <66045653+sadC357@users.noreply.github.com>
2020-08-06 20:32:46 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,19 +2,20 @@ var dog, happyDog;
 var database;
 var foodS,foodStock;
 
-
 function preload(){
-  dogIMG=loadImage("images/dogImg1.png");
-  happyDogIMG=loadImage("images/dogImg.png");
+  dogIMG=loadImage("dogImg1.png");
+  happyDogIMG=loadImage("dogImg.png");
 }
 
 function setup() {
+  database=firebase.database();
   createCanvas(500, 500);
   dog = createSprite(250,250,10,10);
-  foodStock=database.ref('food');
+  dog.addImage(dogIMG);
+  dog.scale=0.3;
+  foodStock=database.ref('Food');
   foodStock.on("value",readStock)
 }
-
 
 function draw() {  
   background(46,139,87);
@@ -23,14 +24,23 @@ function draw() {
     dog.addImage(happyDogIMG);
   }
   drawSprites();
+  fill("black");
+  textSize(25);
+  text("Food Remaining:"+foodS,170,100);
 }
 
 function readStock(data) {
   foodS=data.val();
 }
 
-function writeStock() {
+function writeStock(x) {
+  if (x<=0) {
+    x=0;
+  }
+  else{
+    x=x-1;
+  }
   database.ref('/').update({
-    food:x
+    Food:x
   })
 }
